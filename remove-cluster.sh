d79c83de239f1a3e855d0d361a224d147e000ce2
Marcin Dlugajczyk <dlugajczykmarcin@gmail.com>
2014-07-23 17:22:52 +0100

    Fixed removing cluster.

--- remove-cluster.sh
+++ remove-cluster.sh
@@ -2,18 +2,14 @@
 
 function remove_vm_and_cfg {
     echo "Removing cluster at host: $1"
-    ssh $1 'for vm in $(sudo xl list |grep mpi_node |awk '\''{print $1}'\''); do sudo xl shutdown -w $vm; done '
+    ssh $1 'sudo xl shutdown -a'
     ssh $1 "rm mpi_node*.cfg"
 }
-
-# remove_vm_and_cfg master@compute-021 &
-# remove_vm_and_cfg master@compute-022 &
-# remove_vm_and_cfg master@compute-023 &
-# remove_vm_and_cfg master@compute-024 &
 
 for host in $(cat hosts); do
     remove_vm_and_cfg "master@$host" &
 done
 wait
+sleep 7
 
 
